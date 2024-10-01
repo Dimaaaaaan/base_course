2eb05216eaffaed54f61da971c2eb4433b5b94f6
Dimaaaaaan <dinosv15052008@gmail.com>
2024-10-01 22:10:47 +0000

    ссс

---
+++ task_4.py
@@ -0,0 +1,23 @@
+a = [ 1.5, "Good", "Bad"]
+
+b = [9, "Blue", "Red", 11]
+
+#result_1 = a[1]+ b[3]  ошибка  
+
+#print(result_1)
+
+result_2 = a[2] + b[2]
+
+print(result_2)
+
+result_3 = a[0] * b[0]
+
+print(result_3)
+
+#result_4 = a[1] ** b[3] ошибка
+
+#print(result_4)
+
+result_5 = a + b 
+
+print(result_5)
