5dfad3f70ea146378373e366d5914ea9d5995e25
Dimaaaaaan <dinosv15052008@gmail.com>
2024-10-08 19:47:58 +0000

    доделал

--- extra_task_2.py
+++ extra_task_2.py
@@ -1,1 +1,13 @@
-side1 = int(input)
+side1 = int(input('Введите длину первой стороны: '))
+side2 = int(input('Введите длину второй стороны: '))
+side3 = int(input('Введите длину третьей стороны: '))
+
+if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+    print('Такой треугольник существует.')
+
+    if side1 == side2 == side3:
+        print('Треугольник равносторонний.')
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        print('Треугольник равнобедренный.')
+    else:
+        print('Треугольник разносторонний')
