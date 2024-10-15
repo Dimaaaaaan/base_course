6e8920d64d2c3e89ab0fa097de1ae746dd2841f5
Dimaaaaaan <dinosv15052008@gmail.com>
2024-10-15 11:14:41 +0000

    доделал просто

--- base_task_4.py
+++ base_task_4.py
@@ -1,7 +1,7 @@
 number = int(input('Введите число элементов ряда Фибоначчи: '))
+number_1 = 1
+number_2 = 1
 
-number1 = 0
-number2 = 1
 for _ in range(number):
-    print(number1)
-    number1 = number1 + 5
+    print(number_1 , end=' ')
+    number_1, number_2 = number_2, number_1 + number_2
