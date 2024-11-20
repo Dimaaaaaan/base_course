ca48c9cc45490095f21012341594c1d2447c195f
Dimaaaaaan <dinosv15052008@gmail.com>
2024-11-20 15:18:42 +0000

    рдлплрлл

--- task_6.py
+++ task_6.py
@@ -1,1 +1,17 @@
-list = [age, ]
+list = []
+
+age = input("Введите ваш возраст: ")
+gender = input("Введите ваш пол: ")
+name = input("Введите ваше имя: ")
+city = input("Введите ваш город: ")
+grade = input("В каком классе вы учитесь: ")
+
+list.append(age)
+list.append(gender)
+list.append(name)
+list.append(city)
+list.append(grade)
+
+
+print("Информация о пользователе:")
+print(list)
